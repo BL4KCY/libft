311e9c00bb3236da09d510a3766e5121c06f65b4
mustapha <test@gmail.com>
2023-10-04 23:32:43 +0100

    bonus projects

---
+++ .bonus/ft_lstclear.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 23:07:51 by melfersi          #+#    #+#             */
+/*   Updated: 2023/10/04 23:07:51 by melfersi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	while (lst[0]->next != NULL)
+	{
+		ft_lstdelone(lst[0], del);
+		lst[0] - lst[0]->next;
+	}
+	ft_lstdelone(lst[0], del);
+	lst[0] = NULL;
+}
---
+++ .bonus/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melfersi <melfersi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 15:56:05 by melfersi          #+#    #+#             */
+/*   Updated: 2023/10/03 15:56:05 by melfersi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
